5bd67668db9424f8a5fa2db27dbe77e56cdca655
Oscar Martínez Latasa <omartinezlatasa@gmail.com>
2026-05-10 18:13:42 +0200

    basic demo-ui

--- analytics/app.py
+++ analytics/app.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from services import analyze_events
 
 app = FastAPI(
     title="Telecare Analytics"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
